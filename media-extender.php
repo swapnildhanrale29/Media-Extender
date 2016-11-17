3aff7c6adf66af15fa69049fa7303b7863c56232
Swapnil <swapnildhanrale29@gmail.com>
2016-11-17 08:15:29 +0530

    my secind commit

---
+++ media-extender.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* Plugin Name: Media Extender
+* Plugin URI: http://mypluginuri.com/
+* Description: Extend the media file type.
+* Version: 1.0 
+* Author: Swapnil
+* Author URI: Author's website Dhanrale
+* License: A "Slug" license name e.g. GPL12
+*/
+
+
+// Register the Custom Music Review Post Type
+ 
+ // Multi-language support.
+function enable_language_translation(){
+ load_plugin_textdomain('wp-add-mime-types', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/');
+}
+add_action('plugins_loaded', 'enable_language_translation');
+
+$plugin_basename = plugin_basename ( __FILE__ );
+
+$default_var = array(
+	'wp_add_mime_types'	=>	'2.0.5',
+);
+
+// Add Setting to WordPress 'Settings' menu for Multisite.
+if(is_multisite()){
+	add_action('network_admin_menu', 'network_add_to_settings_menu');
+	require_once( dirname( __FILE__  ) . '/includes/network-admin.php');
+}
+add_action('admin_menu', 'add_to_settings_menu');
+require_once( dirname( __FILE__  ) . '/includes/admin.php');
+
+// Procedure for adding the mime types and file extensions to WordPress.
+function add_allow_upload_extension( $mimes ) {
+	global $plugin_basename;
+	if ( ! function_exists( 'is_plugin_active_for_network' ) ) 
+    	require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+
+	if(is_multisite() && is_plugin_active_for_network($plugin_basename))
+		$settings = get_site_option('wp_add_mime_types_network_array');
+	else
+		$settings = get_option('wp_add_mime_types_array');
+		
+	if(!isset($settings['mime_type_values']) || empty($settings['mime_type_values'])) return $mimes;
+	else
+		$mime_type_values = unserialize($settings['mime_type_values']);
+
+    foreach ($mime_type_values as $line){
+      // If 2 or more "=" character in the line data, it will be ignored.
+      $line_value = explode("=", $line);
+      if(count($line_value) != 2) continue;
+
+      // "　" is the Japanese multi-byte space. If the character is found out, it automatically change the space. 
+      $mimes[trim($line_value[0])] = trim(str_replace("　", " ", $line_value[1])); 
+    }
+    
+    return $mimes;
+}
+
+// Register the Procedure process to WordPress.
+add_filter( 'upload_mimes', 'add_allow_upload_extension');
